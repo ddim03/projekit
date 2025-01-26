3caa28fed21c93b77d6530fe47a0e788f3c2678f
dimasgilang <itsme.dimas03@gmail.com>
2025-01-26 22:34:51 +0700

    feat: ui footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,6 @@
+<footer id="page-footer" class="flex items-center border-t grow-0 border-slate-200 dark:border-slate-700">
+    <div
+        class="container justify-center gap-2 px-4 py-6 text-xs font-medium text-center text-gray-500 lg:px-8 xl:max-w-7xl">
+        <div>© {{ date('Y') }} <span class="font-semibold uppercase">projekit</span></div>
+    </div>
+</footer>
