091e3b208c06dde1f7d038b2a922e76f5980ff16
dimasgilang <itsme.dimas03@gmail.com>
2025-02-04 15:39:35 +0700

    feat(home): add pagination

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -4,13 +4,18 @@ namespace App\Livewire;
 
 use App\Models\Project;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Home extends Component
 {
-
+    use WithPagination;
     public function render()
     {
-        $projects = Project::with('category', 'tags', 'likes', 'user')->cursorPaginate(12);
-        return view('livewire.home', compact('projects'));
+        $projects = Project::with('user:id,name', 'category:id,name')
+            ->withCount('likes')
+            ->orderBy('id', 'desc')
+            ->paginate(12)
+            ->onEachSide(1);
+        return view('livewire.home', ['projects' => $projects]);
     }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,16 +1,10 @@
 <div class="w-full p-4 mx-auto max-w-7xl lg:p-8">
-    <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:gap-8 xl:grid-cols-4">
+    <div class="grid grid-cols-1 gap-4 mb-10 md:grid-cols-2 lg:gap-8 xl:grid-cols-4">
         @forelse ($projects as $project)
         <x-project-card :project="$project" />
         @empty
         <div>No projects found</div>
         @endforelse
     </div>
-    <!-- Load more -->
-    <div class="mt-8 text-center">
-        <button type="button"
-            class="inline-flex items-center justify-center gap-2 px-3 py-2 text-sm font-semibold leading-6 text-gray-800 bg-white border border-gray-300 rounded shadow-sm hover:border-gray-300 hover:bg-gray-100 hover:text-gray-800 hover:shadow focus:outline-none focus:ring focus:ring-gray-500/25 active:border-white active:bg-white active:shadow-none dark:border-gray-700/75 dark:bg-gray-900 dark:text-gray-200 dark:hover:border-gray-700 dark:hover:bg-gray-800 dark:hover:text-gray-200 dark:focus:ring-gray-700 dark:active:border-gray-900 dark:active:bg-gray-900">
-            <span>Load more..</span>
-        </button>
-    </div>
+    {{ $projects->links() }}
 </div>
