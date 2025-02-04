df6d80b7d0b403bf1e8a04b6bcc6545193cf024e
dimasgilang <itsme.dimas03@gmail.com>
2025-02-04 15:41:48 +0700

    fix: hide your projects link if user is not authenticate

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -55,6 +55,7 @@
                     </span>
                     <span class="py-2 grow">Favorite</span>
                 </a>
+                @auth
                 <a href="/favorite"
                     class="group flex items-center gap-2 px-5 py-0.5 text-sm font-medium text-gray-700 hover:bg-blue-50 hover:text-blue-900 active:bg-gray-50 dark:text-gray-200 dark:hover:bg-blue-700 dark:hover:text-blue-200">
                     <span class="flex items-center flex-none opacity-75">
@@ -62,6 +63,7 @@
                     </span>
                     <span class="py-2 grow">Your Projects</span>
                 </a>
+                @endauth
                 <div class="px-5 pt-6 pb-2 text-xs font-medium tracking-wider text-gray-400 uppercase">
                     Category
                 </div>
