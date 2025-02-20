444c0019647555245c691da68ecddc57e8f54263
putraadm <putraadimulya123@gmail.com>
2025-02-20 19:31:24 +0700

    feat: create register page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Livewire\FavoriteProjects;
 use App\Livewire\Home;
+use App\Livewire\ProjectsByCategory;
+use App\Livewire\Register;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class)->name('home');
@@ -9,6 +11,10 @@ Route::get('/', Home::class)->name('home');
 Route::redirect('/category', '/');
 
 Route::get('/category/{slug}', ProjectsByCategory::class)->name('projets-by-category');
+
+Route::get('/project/user', function () {
+    return;
+})->name('projects-by-user');
 
 Route::get('/project/favorite', FavoriteProjects::class)->name('favorite');
 
